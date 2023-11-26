bf327326dbfd151e5ee5e6290c9b50bfcb877b70
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-11-26 14:28:14 +0530

    added some basic styling & created some components

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { PostCard, PostWidget, Categories } from '@/components';
 
 const posts = [
   { title: 'React Testing', excerpt: 'Learn React Testing' },
@@ -16,15 +17,13 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <div key={index}>
-              {post.title}
-              {post.excerpt}
-            </div>
+            <PostCard post={post} key={index} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="relative top-8 lg:sticky">
-            
+            <PostWidget />
+            <Categories />
           </div>
         </div>
       </div>
