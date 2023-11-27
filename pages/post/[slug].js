382ef225f288156d6491ba0719bcef33deaa5e34
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-11-27 15:50:00 +0530

    created new Components and added in slug page

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -8,10 +8,34 @@ import {
   Author,
   Comments,
   CommentsForm,
-} from '@/components';
+} from '../../components';
 
 const PostDetails = () => {
-  return <div>PostDetails</div>;
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          <PostDetail />
+          <Author />
+          <CommentsForm />
+          <Comments />
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostDetails;
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
