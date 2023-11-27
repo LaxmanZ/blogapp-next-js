bd395d735631244e8e07b576e41251f150dc1573
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-11-27 14:37:00 +0530

    designed the Category Component & working on Slug

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { getCetegories } from '@/services';
+import Link from 'next/link';
 
 const Categories = () => {
-  return (
-    <div>Categories</div>
-  )
-}
+  const [categories, setCategories] = useState([]);
 
-export default Categories
+  useEffect(() => {
+    getCetegories().then((newCategories) => setCategories(newCategories));
+  }, []);
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8 pb-12">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
+
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`}>
+          <span className='cursor-pointer block pb-3 mb-3'>{category.name}</span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
---
+++ pages/post/[slug].js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { getPosts, getPostDetails } from '../../services';
+
+import {
+  PostWidget,
+  PostDetail,
+  Categories,
+  Author,
+  Comments,
+  CommentsForm,
+} from '@/components';
+
+const PostDetails = () => {
+  return <div>PostDetails</div>;
+};
+
+export default PostDetails;
