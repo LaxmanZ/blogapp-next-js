daaf98654d4c5a460892d3685e73d5ba20859906
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-11-27 18:55:44 +0530

    designed the commenForm component

--- services/index.js
+++ services/index.js
@@ -133,3 +133,15 @@ export const getCetegories = async () => {
 
   return result.categories;
 };
+
+export const submitComment = async (obj) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
+};
